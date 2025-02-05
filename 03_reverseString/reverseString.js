0c65cf74d67b5cf5b5708d9d213e1f0fa5d96ee6
Andrew Vang <andrw.vang@gmail.com>
2025-02-04 19:45:06 -0600

    Add the first solution to repeatString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let splitText = input.split('').reverse().join('');
+    return splitText;
 };
 
 // Do not edit below this line
