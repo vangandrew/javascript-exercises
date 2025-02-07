ac6b919c47d49a46d98016a1f046ff1c1dadc743
Andrew Vang <andrw.vang@gmail.com>
2025-02-06 21:59:56 -0600

    Add code but not solution to reverseString.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, values) {
+
+    for (let value of values) {
+        const index = array.indexOf(value);
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
