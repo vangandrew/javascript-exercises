e57a15da156483ce4c52cf5f94bbc484e3723180
Andrew Vang <andrw.vang@gmail.com>
2025-02-09 19:23:15 -0600

    Add initial starting point to function, sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (num2 > num1) {
+        for (let i = num1; i <= num2; i++) {
+            sum = sum + i;
+        }
+    }
+
+    if (num1 > num2) {
+        for (let i = num2; i <= num1; i--) {
+            sum = sum + i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
