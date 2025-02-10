f0a7f5e94b25cc5ae2dcedf5838568ab3edacf0f
Andrew Vang <andrw.vang@gmail.com>
2025-02-10 12:04:42 -0600

    Add final solution to sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,29 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
 
+    if (Array.isArray(num1) || Array.isArray(num2)) {
+        return 'ERROR';
+    }
+
     if (num2 > num1) {
         for (let i = num1; i <= num2; i++) {
+            if (num2 < 0 || num1 < 0) {
+                return 'ERROR';
+            } if (!Number.isInteger(num2) || !Number.isInteger(num1)) {
+                return 'ERROR';
+            }
             sum = sum + i;
         }
     }
 
-    if (num1 > num2) {
-        for (let i = num2; i <= num1; i--) {
+    /* Working with larger number first */
+    else if (num1 > num2) {
+        for (let i = num2; i <= num1; i++) {
+            if (num1 < 0 || num2 < 0) {
+                return 'ERROR';
+            } if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+                return 'ERROR';
+            }
             sum = sum + i;
         }
     }
