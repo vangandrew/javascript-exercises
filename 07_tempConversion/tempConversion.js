1573afaec26c12a00a9ff2ccafc67fbc9090ce08
Andrew Vang <andrw.vang@gmail.com>
2025-02-10 13:52:46 -0600

    Add solution to tempConversion function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheight) {
+    const celcius = (fahrenheight - 32) * 5 / 9;
+    celciusRounded = Math.round(celcius * 10) / 10;
+    return celciusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+    const fahrenheit = celcius * (9 / 5) + 32;
+    fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+    return fahrenheitRounded;
 };
 
 // Do not edit below this line
