4404a5d77b34ebf2cf3058f9c5f61c423930980d
Andrew Vang <andrw.vang@gmail.com>
2025-03-22 18:25:37 -0500

    Solve calculator problems up to const(power)

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,21 +1,28 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const sum = array.reduce((total, currentNum) => {
+    return total + currentNum;
+  }, 0)
+
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const total = array.reduce((total, currentNum) => {
+    return total * currentNum;
+  }, 1)
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
 const factorial = function() {
